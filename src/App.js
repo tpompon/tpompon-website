6417466b43c5e8542baa04858605032540a38d83
tpompon <thpompon@gmail.com>
2019-08-05 01:38:52 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
+import './script.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Card from './components/Card'
+
+import { FiMail, FiHeart } from 'react-icons/fi';
+import { FaLinkedinIn, FaTwitter, FaGithub } from 'react-icons/fa';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.pic = React.createRef();
+  }
+
+  componentDidMount() {
+    const pic = this.pic.current;
+    pic.ondragstart = () => { return false; };
+  }
+
+  render () {
+    return (
+      <div className="app">
+        <div className="contact">Contact me</div>
+        <div className="contact-small"><FiMail style={{ marginTop: 6 }} /></div>
+        <img className="pic" src="/me.png" ref={this.pic} />
+        <h1 className="name">Thomas Pompon</h1>
+        <h2 className="bio">20 ans, Étudiant à <a className="italic" href="https://42.fr/" target="_blank">42</a> - Paris</h2>
+        <div className="cards-wrap">
+          <div className="row-cards">
+            <Card
+              title="Twitter Messages"
+              link="https://tpompon.github.io/twitter-messages/"
+              github="https://github.com/tpompon/twitter-messages"
+              picture="/projects-thumbs/twitter-messages.png"
+              desc="A simple front app clone of Twitter Inbox"
+            />
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+          </div>
+          <div className="row-cards">
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+          </div>
+          <div className="row-cards">
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+            <Card
+              title="Projet"
+              picture="/bg.jpg"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            />
+          </div>
+        </div>
+        <footer>
+          Made with <FiHeart style={{ color: 'crimson' }} /> using <a className="italic" href="http://reactjs.org" target="_blank">React.js</a>
+          <div style={{ marginTop: 30 }}>
+            <a href="https://twitter.com/_Tospik" target="_blank" className="social-icon"><FaTwitter /></a>
+            <a href="https://linkedin.com/in/thomaspompon/" target="_blank" className="social-icon"><FaLinkedinIn /></a>
+            <a href="https://github.com/tpompon/" target="_blank" className="social-icon"><FaGithub /></a>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import '../App.css'
+
+import { GoMarkGithub } from "react-icons/go";
+
+class Card extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    const { title, link, github, picture, desc } = this.props;
+    return (
+      <a className="card" href={ link } target="_blank">
+        <img className="card-pic" src={ picture } alt="Screenshot" />
+        <div className="card-body">
+          <div className="row">
+            <h3 className="card-title">{ title }</h3>
+            <a href={ github } target="_blank"><GoMarkGithub className="github-logo" style={{ color: 'black', marginTop: 7 }} /></a>
+          </div>
+          <div className="card-desc">{ desc }</div>
+        </div>
+      </a>
+    )
+  }
+}
+
+export default Card
