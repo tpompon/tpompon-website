fb0d31b3328bec3c09a4948965e3625fb5d40498
tpompon <thpompon@gmail.com>
2019-08-05 13:10:54 +0200

    Updates

---
+++ precache-manifest.977bd9e6a9f137e315f3c5c330b54988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd528fcd48e3d4a4c468089687d1fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987fffd2baa208a5efad",
+    "url": "/static/css/main.b541e92a.chunk.css"
+  },
+  {
+    "revision": "a7fca9f17d9806c356d2",
+    "url": "/static/js/2.cace462f.chunk.js"
+  },
+  {
+    "revision": "987fffd2baa208a5efad",
+    "url": "/static/js/main.7803f766.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
