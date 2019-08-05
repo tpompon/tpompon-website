ca2fe999d3885f113559ec85aaaf81db19aa7bdf
tpompon <thpompon@gmail.com>
2019-08-05 02:11:59 +0200

    Updates

--- precache-manifest.75d023959ad00479bc4070439cc2d00f.js
+++ precache-manifest.75d023959ad00479bc4070439cc2d00f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1a8d2e5a138f9d66efdd0a6cf336a3a4",
+    "revision": "ffd20db137a9b54251d99828a48ff023",
     "url": "/tpompon-website/index.html"
   },
   {
-    "revision": "06d038378c64209e31a4",
+    "revision": "b1da32892d8df5a4997e",
     "url": "/tpompon-website/static/css/main.b541e92a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tpompon-website/static/js/2.cace462f.chunk.js"
   },
   {
-    "revision": "06d038378c64209e31a4",
-    "url": "/tpompon-website/static/js/main.45ecf1c8.chunk.js"
+    "revision": "b1da32892d8df5a4997e",
+    "url": "/tpompon-website/static/js/main.10ee5146.chunk.js"
   },
   {
     "revision": "a2a586e5de16ef5bfde2",
