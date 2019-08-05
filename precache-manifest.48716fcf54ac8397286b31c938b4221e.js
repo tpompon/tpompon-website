20dad37a155c0aff655210858eff3b79571f6c01
tpompon <thpompon@gmail.com>
2019-08-06 00:14:58 +0200

    Updates

--- precache-manifest.48716fcf54ac8397286b31c938b4221e.js
+++ precache-manifest.48716fcf54ac8397286b31c938b4221e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f56af1fedcd7c3ed48cfd8e631ff718f",
+    "revision": "31ab613b7af9fdbb6ec96304a2a7af7c",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.9522b9fe.chunk.css"
   },
   {
-    "revision": "265cd4d66349ada28937",
-    "url": "/static/css/main.85d0428f.chunk.css"
+    "revision": "dc60b182161bf91176a1",
+    "url": "/static/css/main.3300d72f.chunk.css"
   },
   {
     "revision": "f043b81f3c49882bb74a",
     "url": "/static/js/2.2513af18.chunk.js"
   },
   {
-    "revision": "265cd4d66349ada28937",
-    "url": "/static/js/main.18e51729.chunk.js"
+    "revision": "dc60b182161bf91176a1",
+    "url": "/static/js/main.48e51bc5.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74128cf7d54fc0f3d57d8506c09dad50.js"
+  "/precache-manifest.48716fcf54ac8397286b31c938b4221e.js"
 );
 
 self.addEventListener('message', (event) => {
